3957992a16d32ec6f86b8e59dd395c9fc76bfb9b
dmadhu123 <dulikamadushani1234@gmail.com>
2021-06-12 22:27:44 +0530

    Updates

---
+++ precache-manifest.de22263a146ec1508b7610b7ae84b839.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44aedb845d6321071bdb212dbaf097d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c2d71779cd07f5f94f48",
+    "url": "/static/css/main.a90790e1.chunk.css"
+  },
+  {
+    "revision": "94833f0d9aaa3be6210f",
+    "url": "/static/js/2.19e99e5d.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.19e99e5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2d71779cd07f5f94f48",
+    "url": "/static/js/main.2b9246a8.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "8ddc80503edcf5c2268f578188a8cfa1",
+    "url": "/static/media/madhushani.8ddc8050.jpg"
+  },
+  {
+    "revision": "9d3da0e22086c40d5e3106d01c0f6402",
+    "url": "/static/media/resume.9d3da0e2.pdf"
+  }
+]);
